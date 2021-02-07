6c6a6638184faac629c1f74841e15b79cd09862f
Hadi Amjad <46374292+hadiamjad@users.noreply.github.com>
2021-02-07 17:46:04 +0500

    server--tested

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const port = 3000;
 const cors = require('cors');
 const {MongoClient} = require('mongodb');
-const uri="mongodb+srv://hadiyoville25:Hadiharis1@cluster0.26mt4.mongodb.net/NetworkCallStack?retryWrites=true&w=majority";
+const uri="mongodb+srv://hadiyoville25:<password>@cluster0.26mt4.mongodb.net/NetworkCallStack?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
 app.use(cors({credentials: true, origin: true}));
