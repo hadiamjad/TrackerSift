d83d106af187d2502c3e87e4a2ee4a6614e91af4
Hadi Amjad <46374292+hadiamjad@users.noreply.github.com>
2021-02-07 17:13:36 +0500

    server handling updated

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ const bodyParser = require('body-parser');
 const port = 3000;
 const cors = require('cors');
 const {MongoClient} = require('mongodb');
-const uri="mongodb+srv://hadiyoville25:<password>@cluster0.26mt4.mongodb.net/NetworkCallStack?retryWrites=true&w=majority";
+const uri="mongodb+srv://hadiyoville25:Hadiharis1@cluster0.26mt4.mongodb.net/NetworkCallStack?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
 app.use(cors({credentials: true, origin: true}));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 (async function () {
-  
+  await client.connect();
 })();
 
-async function insertRequest(client, newHttpReq){
-  await client.connect();
+async function insertRequest(client, newHttpReq){ 
   const result = await client.db("NetworkCallStack").collection("logs").insertOne(newHttpReq);
   console.log(`New listing created with the following id: ${result.insertedId}`);
-  await client.close();
 }
 
 app.post('/', (req, res) => {
   console.log(req.body);
+  insertRequest(client, req.body);
   res.send("success");
 })
 
