af032558fa5a17b6312bec7612ffbaec7c4131c4
Hadi Amjad <46374292+hadiamjad@users.noreply.github.com>
2021-03-16 23:08:50 +0500

    updated request and response

--- extension/background.js
+++ extension/background.js
@@ -20,9 +20,10 @@ chrome.tabs.query({active:true},
     return;
 
   if (message == "Network.requestWillBeSent") {
-    fetch("http://localhost:3000/", {
+    fetch("http://localhost:3000/request", {
       method: "POST", 
       body: JSON.stringify({"http_req": params.request.url,
+      "request_id":params.requestId,
       "top_level_url":params.documentURL,
       "resource_type":params.type,
       "header": params.request.headers,
@@ -36,7 +37,33 @@ chrome.tabs.query({active:true},
       console.log("Request complete! response");
     });
 
-  } 
+  }
+  else if (message == "Network.responseReceived") {
+      chrome.debugger.sendCommand({
+          tabId: tabId
+      }, "Network.getResponseBody", {
+          "requestId": params.requestId
+      }, function(response) {
+              // you get the response body here!
+              fetch("http://localhost:3000/response", {
+                method: "POST", 
+                body: JSON.stringify({
+                "request_id":params.requestId,
+                "response":params.response,
+                "response_body":response,
+                "resource_type":params.type}),
+                mode: 'cors',
+                headers: {
+                  'Access-Control-Allow-Origin':'*',
+                  "Content-Type": "application/json"
+                }
+              }).then(res => {
+                console.log("Response complete! response");
+              });
+      });
+    
+
+  }
 }
 
 
